9f2f9fcd219ada89fccf60618ce6f060c59d33df
Alessandro-Prudente-Peano-4A <56470974+Alessandro-Prudente-Peano-4A@users.noreply.github.com>
2020-03-30 19:31:38 +0200

    Create SpaceInvader.java

---
+++ SpaceInvader.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinveder;
+
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author prudente.alessandro
+ */
+public class SpaceInvader extends JFrame {
+
+    public SpaceInvader() throws InterruptedException {
+       
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Intro intro = new Intro();
+        this.add(intro); // impacchetta e rende visibile il tutto
+        this.pack();
+        this.setVisible(true);
+        intro.run();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpaceInvader giochino=new SpaceInvader();
+        Space spazio=new Space();
+    }
+
+}
